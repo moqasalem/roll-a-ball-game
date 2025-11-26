b518dffeeda70d512efb299d48430c54e69df11e
Salem Moqa <moqasalem@gmail.com>
2025-11-26 14:36:22 +0300

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+
+    // Speed at which the player moves.
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public GameObject playAgainObject;
+
+    // Rigidbody of the player.
+    private Rigidbody rb;
+    private int count;
+
+
+    // Movement along X and Y axes.
+    private float movementX;
+    private float movementY;
+
+
+
+    // Start is called before the first frame update.
+    void Start()
+    {
+        // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        playAgainObject.SetActive(false);
+    }
+
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
+    {
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //not working here
+            //loseTextObject.SetActive(true);   // إظهار رسالة الخسارة
+            //Destroy(gameObject);
+            loseTextObject.SetActive(true);
+            Time.timeScale = 0f;
+            playAgainObject.SetActive(true);
+
+        }
+    }
+
+
+    public void PlayAgain()
+    {
+        Time.timeScale = 1f; // إعادة الوقت
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // إعادة تحميل المشهد
+    }
+}
