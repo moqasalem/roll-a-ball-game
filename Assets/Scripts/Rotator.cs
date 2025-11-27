730b6554402efce1c1655bc250e264dba00e09c4
Salem Moqa <moqasalem@gmail.com>
2025-11-27 12:12:54 +0300

    Update project files to change analyzer paths and add new Unity references; modify MiniGame scene to include Unity Recorder bindings; clean up Rotator script formatting; adjust ReadmeEditor script structure; change fullscreen mode in project settings.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-   
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
